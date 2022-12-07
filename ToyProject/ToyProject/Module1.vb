75639593c980e8035d3f756793a21ac70f1499df
rmcanany <64286960+rmcanany@users.noreply.github.com>
2022-12-07 11:27:40 -0600

    Replaced sleep statement with readkey

--- ToyProject/ToyProject/Module1.vb
+++ ToyProject/ToyProject/Module1.vb
@@ -4,7 +4,10 @@
         Console.WriteLine("Hello world from Robert!") 'Francesco was here
         Console.WriteLine("Hello world from Bobbery!")
         Console.WriteLine("Hello world from Francesco!")
-        System.Threading.Thread.Sleep(5000)
+
+        Dim msg As String = "Press any key to exit..."
+        Console.WriteLine(msg)
+        Console.ReadKey(True)
     End Sub
 
 End Module
