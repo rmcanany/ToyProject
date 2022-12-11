e9c3847ca01f1294c83ac2dfe36ab18722a1dae5
Francesco <francesco.arfilli@gmail.com>
2022-12-11 20:12:22 +0100

    Awesome changes!
    Shiny code inserted

--- ToyProject/ToyProject/Module1.vb
+++ ToyProject/ToyProject/Module1.vb
@@ -5,6 +5,8 @@
         Console.WriteLine("Hello world from Bobbery!")
         Console.WriteLine("Hello world from Francesco!")
 
+        Console.WriteLine("This is a great branch!")
+
         Dim msg As String = String.Format("{0}Press any key to exit...", vbCrLf)
         Console.WriteLine(msg)
         Console.ReadKey(True)
